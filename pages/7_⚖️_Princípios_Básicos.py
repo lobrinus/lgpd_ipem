4bce2ca6e55f30dc68503978cf7868ba79d5a647
lobrinus <129767161+lobrinus@users.noreply.github.com>
2025-05-16 09:59:18 -0300

    Update 7_⚖️_Princípios_Básicos.py

--- pages/7_⚖️_Princípios_Básicos.py
+++ pages/7_⚖️_Princípios_Básicos.py
@@ -1,10 +1,11 @@
 import streamlit as st
+from login import exibir_login
 
 st.set_page_config(
     page_title="Princípios Básicos - LGPD IPEM-MG",
     page_icon="⚖️"
 )
-
+exibir_login()
 st.title("⚖️ Princípios Básicos da LGPD")
 st.markdown("---")
 st.markdown("""
