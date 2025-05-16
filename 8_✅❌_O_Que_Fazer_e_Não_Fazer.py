e8e3a3b5ee81a9ec352bf6cf9571d1d95e8d14d2
lobrinus <129767161+lobrinus@users.noreply.github.com>
2025-05-16 13:17:04 -0300

    Update 8_✅❌_O_Que_Fazer_e_Não_Fazer.py

--- 8_✅❌_O_Que_Fazer_e_Não_Fazer.py
+++ 8_✅❌_O_Que_Fazer_e_Não_Fazer.py
@@ -1,10 +1,7 @@
 import streamlit as st
 from login import exibir_login
 
-st.set_page_config(
-    page_title="O Que Fazer e Não Fazer - LGPD IPEM-MG",
-    page_icon="✅❌"
-)
+
 st.title("✅❌ O Que Fazer e Não Fazer")
 st.markdown("---")
 st.markdown("""
