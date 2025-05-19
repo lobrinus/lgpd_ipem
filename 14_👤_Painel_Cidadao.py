eabdb10cd6897b8544bbb274da299f554a1c7308
lobrinus <129767161+lobrinus@users.noreply.github.com>
2025-05-19 08:17:30 -0300

    Update 14_👤_Painel_Cidadao.py

--- 14_👤_Painel_Cidadao.py
+++ 14_👤_Painel_Cidadao.py
@@ -5,6 +5,20 @@ from firebase_admin import credentials, firestore
 import datetime
 import json
 from firebase_config import auth
+import pyrebase
+
+firebaseConfig = {
+    "apiKey": "AIzaSyB5chTFihZM_v-5bkVecmDDUvkOKG7C22Q",
+    "authDomain": "lgpd-ipem-mg-9f1a5.firebaseapp.com",
+    "projectId": "lgpd-ipem-mg-9f1a5",
+    "storageBucket": "lgpd-ipem-mg-9f1a5.appspot.com",
+    "messagingSenderId": "XXXXXXXXXXXX",
+    "appId": "1:XXXXXXXXXXXX:web:XXXXXXXXXXXX",
+    "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 # 🔐 Firebase Web Config (API Key)
 if not firebase_admin._apps:
