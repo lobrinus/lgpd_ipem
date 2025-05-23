31c62fb17ad469500c2a448d93b52483ae17b174
lobrinus <129767161+lobrinus@users.noreply.github.com>
2025-05-23 10:59:37 -0300

    Update orientacao_dados.py

--- orientacao_dados.py
+++ orientacao_dados.py
@@ -55,9 +55,11 @@ def render():
     """)
     
     # Rodapé
+    st.markdown("---")
     st.markdown("""
-    <hr>
-    <p style="text-align: center; color: gray;">
-        © 2025 IPEM-MG. Promovendo privacidade e segurança de dados. Todos os Direitos Reservados.
-    </p>
+    <div style="text-align: center; color: gray; margin-top: 40px;">
+        © 2025 IPEM-MG. Todos os direitos reservados.<br>
+        R. Cristiano França Teixeira Guimarães, 80 - Cinco, Contagem - MG, 32010-130<br> 
+        CNPJ: 17.322.264/0001-64 | Telefone:  (31) 3399-7134 / 08000 335 335
+    </div>
     """, unsafe_allow_html=True)
