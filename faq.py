c7044ae5fac2c15c34da7c2b4b2baca8b665a1cf
lobrinus <129767161+lobrinus@users.noreply.github.com>
2025-05-23 11:26:49 -0300

    Update faq.py

--- faq.py
+++ faq.py
@@ -13,7 +13,8 @@ def render():
     for imagem in imagens:
         col1, col2, col3 = st.columns([1, 2, 1])
         with col2:
-            st.image(imagem, use_container_width=True
+            st.image(imagem, use_container_width=True)  # <- Parêntese fechado aqui
+
     # Rodapé
     st.markdown("---")
     st.info("**Não encontrou sua dúvida?** Entre em contato com nosso Encarregado de Dados: encarregado.data@ipem.mg.gov.br")
@@ -22,6 +23,6 @@ def render():
     <div style="text-align: center; color: gray; margin-top: 40px;">
         © 2025 IPEM-MG. Todos os direitos reservados.<br>
         R. Cristiano França Teixeira Guimarães, 80 - Cinco, Contagem - MG, 32010-130<br> 
-        CNPJ: 17.322.264/0001-64 | Telefone:  (31) 3399-7134 / 08000 335 335
+        CNPJ: 17.322.264/0001-64 | Telefone: (31) 3399-7134 / 08000 335 335
     </div>
     """, unsafe_allow_html=True)
